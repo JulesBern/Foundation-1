7968d15f643be49f7d8859bf290ea4514bbd08df
JulesBern <bern.jules@gmail.com>
2013-09-19 12:23:20 -0400

    Update footer.php
    To properly show 4 column widget.

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 <!-- Footer -->
 <footer class="row">
 
-<?php if ( dynamic_sidebar('Sidebar Footer One') || dynamic_sidebar('Sidebar Footer Two') || dynamic_sidebar('Sidebar Footer Three') || dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar Footer One') || !dynamic_sidebar('Sidebar Footer Two') || !dynamic_sidebar('Sidebar Footer Three') || !dynamic_sidebar('Sidebar Footer Four')) : ?>
 
 <div class="large-12 columns">
 	<ul class="inline-list">
